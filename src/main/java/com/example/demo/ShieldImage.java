fcf7d0dd0163490b50da619a6783d63a28b9f4f3
TimZ21 <shuning512@gmail.com>
2024-11-05 14:52:50 +0800

    Fix: Add null check to prevent NullPointerException when loading image

--- src/main/java/com/example/demo/ShieldImage.java
+++ src/main/java/com/example/demo/ShieldImage.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+import java.util.Objects;
+
 public class ShieldImage extends ImageView {
 	
 	private static final String IMAGE_NAME = "/images/shield.png";
@@ -12,7 +14,7 @@ public class ShieldImage extends ImageView {
 		this.setLayoutX(xPosition);
 		this.setLayoutY(yPosition);
 		//this.setImage(new Image(IMAGE_NAME));
-		this.setImage(new Image(getClass().getResource(IMAGE_NAME).toExternalForm()));
+		this.setImage(new Image(Objects.requireNonNull(getClass().getResource(IMAGE_NAME)).toExternalForm()));
 		this.setVisible(false);
 		this.setFitHeight(SHIELD_SIZE);
 		this.setFitWidth(SHIELD_SIZE);
