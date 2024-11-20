cf68e05c7d11e28cdd92406f193e9b666511aec5
TimZ21 <shuning512@gmail.com>
2024-11-20 18:57:06 +0800

    fix: fix the bug that user plane and boss can fly over the screen and can't reach the lower boundary of screen
    - Modifying the iamge and height of UserPlane and Boss cause this bug
    
    Fix this bug by adjusting Y_UPPER_BOUND and Y_LOWER_BOUND to correct value

--- src/main/java/com/example/demo/actors/plane/UserPlane.java
+++ src/main/java/com/example/demo/actors/plane/UserPlane.java
@@ -6,8 +6,8 @@ import com.example.demo.actors.projectile.UserProjectile;
 public class UserPlane extends FighterPlane {
 
 	private static final String IMAGE_NAME = "userplane.png";
-	private static final double Y_UPPER_BOUND = -40;
-	private static final double Y_LOWER_BOUND = 600.0;
+	private static final double Y_UPPER_BOUND = 0;
+	private static final double Y_LOWER_BOUND = 665;
 	private static final double INITIAL_X_POSITION = 5.0;
 	private static final double INITIAL_Y_POSITION = 300.0;
 	private static final int IMAGE_HEIGHT = 50;
@@ -21,7 +21,7 @@ public class UserPlane extends FighterPlane {
 		super(IMAGE_NAME, IMAGE_HEIGHT, INITIAL_X_POSITION, INITIAL_Y_POSITION, initialHealth);
 		velocityMultiplier = 0;
 	}
-	
+
 	@Override
 	public void updatePosition() {
 		if (isMoving()) {
@@ -33,12 +33,12 @@ public class UserPlane extends FighterPlane {
 			}
 		}
 	}
-	
+
 	@Override
 	public void updateActor() {
 		updatePosition();
 	}
-	
+
 	@Override
 	public ActiveActorDestructible fireProjectile() {
 		return new UserProjectile(PROJECTILE_X_POSITION, getProjectileYPosition(PROJECTILE_Y_POSITION_OFFSET));
