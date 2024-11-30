5d5efbf497dad69ad250ca03f82915eba41f01f9
TimZ21 <shuning512@gmail.com>
2024-11-30 21:51:33 +0800

    refactor(GameOverImage): remove GameOverImage class, corresponding rescource and references
    - Due to the introduction of the LoseMenu, the GameOverImage class and its uses in LevelView become useless and redundant.
    
    - This commit removes the GameOverImage class and all related code in LevelView to clean up the project structure and eliminate unused code
    
    - Delete the game over image file beacuse it is unused after deletion of the GameOverImage class
    
    - The LoseMenu now handles all functionalities prepreviously covered by the GameOverImage, and also add more functionalties for better game flow

--- src/main/java/com/example/demo/view/LevelView.java
+++ src/main/java/com/example/demo/view/LevelView.java
@@ -14,7 +14,6 @@ public class LevelView {
 	private static final int LOSS_SCREEN_X_POSITION = -160;
 	private static final int LOSS_SCREEN_Y_POSITION = -375;
 	private final Group root;
-	private final GameOverImage gameOverImage;
 	private final HeartDisplay heartDisplay;
 
 	/**
@@ -26,7 +25,6 @@ public class LevelView {
 	public LevelView(Group root, int heartsToDisplay) {
 		this.root = root;
 		this.heartDisplay = new HeartDisplay(HEART_DISPLAY_X_POSITION, HEART_DISPLAY_Y_POSITION, heartsToDisplay);
-		this.gameOverImage = new GameOverImage(LOSS_SCREEN_X_POSITION, LOSS_SCREEN_Y_POSITION);
 	}
 
 	/**
@@ -36,13 +34,6 @@ public class LevelView {
 		root.getChildren().add(heartDisplay.getContainer());
 	}
 
-
-	/**
-	 * Displays the game over image on the screen by adding it to the root group.
-	 */
-	public void showGameOverImage() {
-		root.getChildren().add(gameOverImage);
-	}
 
 	/**
 	 * Updates the heart display by removing hearts based on the number of hearts remaining.
